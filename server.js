7ca78eceb137bce0e0cac300d753c7fd9160d94b
Sourabh <chanchalbhardwaj0525@gmail.com>
2023-04-14 17:07:53 +0530

    intial commit models are created

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DPASS);
+
+const restaurantRoute= require("./routes/restaurant.routes")
+
+const app = express()
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+  useUnifiedTopology: true,
+}).then(()=>console.log("Db connection sucessful"))
+
+
+app.use(express.json())
+
+app.use("/api/restaurant",restaurantRoute)
+
+
+app.listen(process.env.PORT,()=>{
+  console.log(`server is Up and runing on Port ${process.env.PORT}`)
+})
